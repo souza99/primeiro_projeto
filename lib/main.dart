a2918caa27ac05d25f2ab69ce6934089f749059c
souza99 <jpfsouza99@gmail.com>
2022-11-29 18:54:12 -0300

    Passando funções de varias formas

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  
   void responder() {
     print("Pergunta respondida");
   }
-  
+
+  // void Function() funcaoQueRetornaUmaFuncao() {
+  //   return () {
+  //     print('Pergunta Respondida #02');
+  //   };
+  // }
+
   final List<String> perguntas = [
     'Qual a sua cor favorita ?',
     'Qual é o seu animal favorito ?',
@@ -25,12 +30,23 @@ class PerguntaApp extends StatelessWidget {
               Text(perguntas.elementAt(0)),
               ElevatedButton(
                 child: Text('Resposta 1'),
-                onPressed: responder(),
+                onPressed: responder,
               ),
-              ElevatedButton(child: Text('Resposta 2'), onPressed: responder()
+              // Utilizando uma funcao que retorna outra função
+              // ElevatedButton(
+              //   child: Text('Resposta 2'),
+              //   onPressed: funcaoQueRetornaUmaFuncao(),
+              // ),
+
+              ElevatedButton(
+                child: Text('Resposta 2'),
+                onPressed: () {
+                  print("Resposta 2 foi selecionada! ");
+                },
               ),
-              ElevatedButton(child: Text('Resposta 3'), onPressed: responder()
-              ),
+              ElevatedButton(
+                  child: Text('Resposta 3'),
+                  onPressed: () => print("Resposta 3!!!")),
             ],
           )),
     );
@@ -43,3 +59,7 @@ class PerguntaApp extends StatelessWidget {
 // estou montando minha tela no Scaffold
 
 // column serve para organizar o conteudo na tela
+
+// quando passamos a funcao com "()", tipo "responder()" estamos evocando ela
+// quando passamos ela sem "()", tipo "responder" passamos ela como parametro
+// no caso de Widgets, o que passamos dentro deles, são parametros
